c2a9378dc7cdd4f7d172134a9c086165695e2ad7
mister-abdurahman <aramramadan7@gmail.com>
2023-01-24 09:02:32 +0100

    .

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
-const http = require("http");
-const url = require("url");
+// const http = require("http");
+// const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
+const express = require("express");
 
 // SERVER
 // NB: we must set headers before sending the response
+const app = express();
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -22,47 +24,96 @@ const templateProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url); //the url in request. favicon is requested by default (not important)
+// const server = http.createServer((req, res) => {
+//   // console.log(req.url); //the url in request. favicon is requested by default (not important)
 
-  // we need to parse our url to get access to the req data in a nice object format
-  const { query, pathname } = url.parse(req.url, true);
+//   // we need to parse our url to get access to the req data in a nice object format
+//   const { query, pathname } = url.parse(req.url, true);
 
-  // Overview page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+//   // Overview page
+//   if (pathname === "/" || pathname === "/overview") {
+//     res.writeHead(200, { "Content-type": "text/html" });
+//     const cardsHtml = dataObj
+//       .map((el) => replaceTemplate(templateCard, el))
+//       .join("");
+//     const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+//     res.end(output);
+//   }
+
+//   // Product page
+//   else if (pathname === "/product") {
+//     res.writeHead(200, { "Content-type": "text/html" });
+//     const product = dataObj[query.id];
+//     const output = replaceTemplate(templateProduct, product);
+//     res.end(output);
+//   }
+
+//   // API page
+//   else if (pathname === "/api") {
+//     // dirname is where the current file is located. (req fn is an exception)
+//     res.writeHead(200, { "Content-type": "application/json" });
+//     res.end(data);
+//   }
+//   // Not Found page
+//   else {
+//     res.writeHead(404, {
+//       "Content-type": "text/html",
+//       "a-custom-header": "hello-world",
+//     });
+//     res.end("<h1>Page not found</h1>");
+//   }
+// });
+
+//////////////////////////
+
+app.get("/", async (req, res, next) => {
+  // res.writeHead(200, { "Content-type": "text/html" });
+  try {
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(templateCard, el))
       .join("");
     const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-
-    res.end(output);
-  }
-
-  // Product page
-  else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
-    const product = dataObj[query.id];
-    const output = replaceTemplate(templateProduct, product);
-    res.end(output);
-  }
-
-  // API page
-  else if (pathname === "/api") {
-    // dirname is where the current file is located. (req fn is an exception)
-    res.writeHead(200, { "Content-type": "application/json" });
-    res.end(data);
-  }
-  // Not Found page
-  else {
-    res.writeHead(404, {
-      "Content-type": "text/html",
-      "a-custom-header": "hello-world",
-    });
-    res.end("<h1>Page not found</h1>");
+    res.status(201).end(output);
+  } catch (error) {
+    next();
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
+app.get("/overview", async (req, res, next) => {
+  // res.writeHead(200, { "Content-type": "text/html" });
+  try {
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(templateCard, el))
+      .join("");
+    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.status(201).end(output);
+  } catch (error) {
+    next();
+  }
+});
+
+app.get("/product", async (req, res, next) => {
+  // res.writeHead(200, { "Content-type": "text/html" });
+  try {
+    const id = req.query.id;
+    const product = dataObj[id];
+    const output = replaceTemplate(templateProduct, product);
+    res.status(201).end(output);
+  } catch (error) {
+    next();
+  }
+});
+
+// Error Handling
+app.use((err, req, res, next) => {
+  const message = err.message || "<h1>Page not found</h1>";
+  // return res.status(404).json({ message });
+  return res.status(404).end(message);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
 });
